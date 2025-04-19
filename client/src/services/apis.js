4c6de6a20ab502f771393a84929afdcdb10fe0c1
Dev Kushwah <devkushwah880@gmail.com>
2025-04-19 18:21:26 -0400

    Add API's for Myprofile, Setting and EnrolledCourse

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -18,3 +18,18 @@ export const categories = {
 export const contactusEndpoint = {
   CONTACT_US_API: BASE_URL + "/reach/contact",
 }
+
+// PROFILE ENDPOINTS
+export const profileEndpoints = {
+  GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
+  GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
+  GET_INSTRUCTOR_DATA_API: BASE_URL + "/profile/instructorDashboard",
+}
+
+// SETTINGS PAGE API
+export const settingsEndpoints = {
+  UPDATE_DISPLAY_PICTURE_API: BASE_URL + "/profile/updateDisplayPicture",
+  UPDATE_PROFILE_API: BASE_URL + "/profile/updateProfile",
+  CHANGE_PASSWORD_API: BASE_URL + "/auth/changepassword",
+  DELETE_PROFILE_API: BASE_URL + "/profile/deleteProfile",
+}

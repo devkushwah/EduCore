c973aa8c67c741c579a59fdbb951db10e0c92112
Dev Kushwah <devkushwah880@gmail.com>
2025-04-25 08:49:20 -0400

    Create ViewCourse Page

---
+++ client/src/Pages/ViewCourse.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useParams } from "react-router-dom";
+
+import CourseReviewModal from "../components/core/ViewCourse/CourseReviewModal";
+import VideoDetailsSidebar from "../components/core/ViewCourse/VideoDetailsSidebar";
+import { getFullDetailsOfCourse } from "../services/operations/courseDetailsAPI";
+import {
+  setCompletedLectures,
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+} from "../slices/viewCourseSlice";
+
+export default function ViewCourse() {
+  const { courseId } = useParams();
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [reviewModal, setReviewModal] = useState(false);
+  const fetched = useRef(false);  // Avoid multiple API calls in StrictMode
+
+  useEffect(() => {
+    if (fetched.current) return;
+    fetched.current = true;
+
+    (async () => {
+      try {
+        const courseData = await getFullDetailsOfCourse(courseId, token);
+        if (!courseData || !courseData.courseDetails) return;
+
+        dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
+        dispatch(setEntireCourseData(courseData.courseDetails));
+        dispatch(setCompletedLectures(courseData.completedVideos));
+
+        let lectures = 0;
+        courseData?.courseDetails?.courseContent?.forEach((sec) => {
+          lectures += sec?.subSection?.length || 0;
+        });
+        dispatch(setTotalNoOfLectures(lectures));
+      } catch (error) {
+        console.error("Error fetching course details:", error);
+      }
+    })();
+  }, [courseId, token]);  // Dependencies added
+
+  return (
+    <>
+      <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+        <VideoDetailsSidebar setReviewModal={setReviewModal} />
+        <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+          <div className="mx-6">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+      {reviewModal && <CourseReviewModal setReviewModal={setReviewModal} />}
+    </>
+  );
+}

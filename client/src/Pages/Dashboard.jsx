a1ae2fa32b9124d15256bdb66d29f32f4db98fc7
Dev Kushwah <devkushwah880@gmail.com>
2025-04-27 09:10:46 -0400

    Import Sidebar Dasboard Component

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Sidebar from '../components/core/Dashboard/Sidebar'
+import Sidebar from '../components/core/Dashboard/Sidebar'
 import { Outlet } from 'react-router-dom'
 import { useSelector } from "react-redux"
 
@@ -18,7 +18,7 @@ const Dashboard = () => {
 
     return (
         <div className="relative flex min-h-[calc(100vh-3.5rem)]">
-          {/* <Sidebar /> */}
+          <Sidebar />
           <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
             <div className="mx-auto w-11/12 max-w-[1000px] py-10">
               <Outlet />

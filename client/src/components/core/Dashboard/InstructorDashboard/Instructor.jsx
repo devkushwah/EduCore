1e1fa59930649c5937c67cf4fc9ce2d8160a7c43
Dev Kushwah <devkushwah880@gmail.com>
2025-04-28 10:47:53 -0400

    Create Instructor Dashboard

---
+++ client/src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Instructor = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-richblack-5 font-semibold'>Instructor Dashboard</h1>
+      <p className='text-lg text-richblack-300 mt-2'>Manage your courses and students from here.</p>
+      <div className='mt-10'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Instructor
